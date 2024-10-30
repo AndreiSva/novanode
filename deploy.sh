87eafc3261e3ea54634ca40850e837f61c84889f
Andrei <andreisva2023@gmail.com>
2024-10-30 00:32:37 -0700

    deploy.sh: fix deployment

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 npm run build
-rsync -ar --progress ./build andrei@rm-r.org:/var/www/andrei.rm-r.org/
-rsync -ar --progress ./static andrei@rm-r.org:/var/www/andrei.rm-r.org/
+rsync -f --ignore-existing -ar --progress ./build andrei@rm-r.org:/var/www/andrei.rm-r.org/
+rsync -f --ignore-existing -ar --progress ./static andrei@rm-r.org:/var/www/andrei.rm-r.org/
 systemctl --host=root@rm-r.org restart AndreiSite
